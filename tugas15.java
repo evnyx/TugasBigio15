e5971f656c78b178bd4baf264d06571fd613ca8c
Ikhlaashul Mu'aasyiqiin <39987415+evnyx@users.noreply.github.com>
2021-09-01 16:11:15 +0700

    Add files via upload

---
+++ tugas15.java
@@ -0,0 +1,49 @@
+package Tugas15;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+public class tugas15 {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Jumlah test case: ");
+        int test_cases = kb.nextInt();
+        for (int cs = 1; cs <= test_cases; cs++) {
+            System.out.print("Jumlah elemen dalam array: ");
+            int n = kb.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                System.out.print("Element array ke-" + (i + 1) + ": ");
+                a[i] = kb.nextInt();
+            }
+            findZigZagSequence(a, n);
+        }
+    }
+
+    public static void findZigZagSequence(int[] a, int n) {
+        Arrays.sort(a);
+        int mid = (n) / 2;
+        int temp = a[mid];
+        a[mid] = a[n - 1];
+        a[n - 1] = temp;
+
+        int st = mid + 1;
+        int ed = n - 2; 
+        while (st <= ed) {
+            temp = a[st];
+            a[st] = a[ed];
+            a[ed] = temp;
+            st = st + 1;
+            ed = ed - 1;
+        }
+        for (int i = 0; i < n; i++) {
+            if (i > 0)
+                System.out.print(" ");
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+}
